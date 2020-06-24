bc1d8def126985bb04c155f39ec5eafdb07edaf2
Mikhail <pmp@nopreset.ru>
2020-06-24 16:57:12 -0400

    icon change

--- src/client/react/components/icons/dots.js
+++ src/client/react/components/icons/dots.js
@@ -12,21 +12,9 @@ class Dots extends Component {
       height="4"
       viewBox="0 0 22 4"
     >
-      <defs>
-        <linearGradient
-          id="linearGradient-1"
-          x1="50%"
-          x2="50%"
-          y1="4.515%"
-          y2="98.645%"
-        >
-          <stop offset="0%"></stop>
-          <stop offset="100%" stopColor="#30220D"></stop>
-        </linearGradient>
-      </defs>
+     
       <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
         <g transform="translate(-331 -66)">
-          <path fill="url(#linearGradient-1)" d="M-62 9H390V534H-62z"></path>
           <g fill="#FFF" transform="translate(331 66)">
             <path d="M2.5 0a2 2 0 110 4 2 2 0 010-4zm8.25 0a2 2 0 110 4 2 2 0 010-4zm8.667 0a2 2 0 110 4 2 2 0 010-4z"></path>
           </g>
